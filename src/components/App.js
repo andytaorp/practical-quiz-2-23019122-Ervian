82b71b5a6da746a80742957337d63c5d07c29bfc
23019122- Ervian <23019122@myrp.edu.sg>
2024-11-27 15:51:37 +0800

    Updated Files

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,29 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
     // TODO: write code to add a new task
   };
 
   const handleToggleTask = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, finished: !task.finished } : task
+      )
+    );
     // TODO: write code to toggle a task's status
   };
 
   const handleDeleteTask = (id) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
     // TODO: write code to delete a task
   };
 
   return (
     <div className="app">
       <h1>Task Tracker</h1>
+      <Form onAddTask={handleAddTask} />
+      <TaskList tasks={tasks} onToggleTask={handleToggleTask} onDeleteTask={handleDeleteTask} />
       {/*TODO: add a form to add a new task*/}
       {/*TODO: add a list of tasks*/}
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,26 @@ export default function Form({ onAddTask }) {
   const [description, setDescription] = useState("");
 
   const handleSubmit = (e) => {
+      e.preventDefault();
+      const newTask = { id: Date.now(), description, finished: false };
+      onAddTask(newTask);
+      setDescription("");
+  
     // TODO: write code to handle form submission
   };
 
-  return {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Add Task</h3>
+    <input
+    type="text"
+    placeholder="Add a task"
+    value={description}
+    onChange={(e) => setDescription(e.target.value)}
+    />
+    <button type="submit">Add Task</button>
+    </form>
     /*TODO: add a form to add a new task*/
+  )
   };
-}
+
